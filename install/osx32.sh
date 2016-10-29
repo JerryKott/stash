cac10c92d9c8c5736b066201af6a3cace7ad4ec7
JerryKott <jkott@image-ware.com>
2016-10-28 20:36:36 -0700

    Fixed download urls

--- install/osx32.sh
+++ install/osx32.sh
@@ -6,16 +6,16 @@ echo $TEMP
 pushd $TEMP
 
 #Get and unpack the VM
-curl -o bin.zip https://github.com/JerryKott/stash/blob/master/bin/macx.zip
+curl -o bin.zip https://raw.githubusercontent.com/JerryKott/stash/master/bin/macx.zip
 unzip bin.zip
 chmod a+x macx/stash32
 
 #Get and unpack the stash image
-curl -o image.zip https://github.com/JerryKott/stash/blob/master/image/image.zip
+curl -o image.zip https://raw.githubusercontent.com/JerryKott/stash/master/image/image.zip
 unzip image.zip
 
 #Get and unpack the installation script
-curl -o script.zip https://github.com/JerryKott/stash/blob/master/scripts/macx.zip
+curl -o script.zip https://raw.githubusercontent.com/JerryKott/stash/master/scripts/macx.zip
 unzip script.zip
 chmod a+x macx/stash
 
@@ -27,7 +27,7 @@ cp -a macx/stash $TARGET/
 cp -a stash.im $TARGET/
 
 #Get and unpack examples.
-curl -o examples.zip https://github.com/JerryKott/stash/blob/master/examples.zip
+curl -o examples.zip https://raw.githubusercontent.com/JerryKott/stash/master/examples.zip
 unzip examples.zip
 mv examples $TARGET/
 
