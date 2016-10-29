674925a5d7ac6b8178741d46afa6b2040cee5f30
JerryKott <jkott@image-ware.com>
2016-10-28 20:10:41 -0700

    First attempt at installation

---
+++ install/linux64.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+TEMP=`mktemp -d -t 'stash'`
+echo $TEMP
---
+++ install/osx32.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Create a temp directory first
+TEMP=`mktemp -d -t 'stash'`
+echo $TEMP
+pushd $TEMP
+
+#Get and unpack the VM
+curl -o bin.zip https://github.com/JerryKott/stash/tree/master/bin/macx.zip
+unzip bin.zip
+chmod a+x macx/stash32
+
+#Get and unpack the stash image
+curl -o image.zip https://github.com/JerryKott/stash/tree/master/image/image.zip
+unzip image.zip
+
+#Get and unpack the installation script
+curl -o script.zip https://github.com/JerryKott/stash/tree/master/scripts/macx.zip
+unzip script.zip
+chmod a+x macx/stash
+
+
+TARGET=/usr/local/stash
+mkdir $TARGET
+cp -a macx/stash32 $TARGET/macx/
+cp -a macx/stash $TARGET/
+cp -a stash.im $TARGET/
+
+#Get and unpack examples.
+curl -o examples.zip https://github.com/JerryKott/stash/tree/master/examples.zip
+unzip examples.zip
+mv examples $TARGET/
+
+rm -rf *
+
+popd
