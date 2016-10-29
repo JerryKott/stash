9b63a261aba280d3e18d2bc26dc9dbd75491fbea
JerryKott <jkott@image-ware.com>
2016-10-28 20:26:00 -0700

    Added missing examples.zip and fixed paths

--- install/osx32.sh
+++ install/osx32.sh
@@ -11,11 +11,11 @@ unzip bin.zip
 chmod a+x macx/stash32
 
 #Get and unpack the stash image
-curl -o image.zip https://github.com/JerryKott/stash/tree/master/image/image.zip
+curl -o image.zip https://github.com/JerryKott/stash/blob/master/image/image.zip
 unzip image.zip
 
 #Get and unpack the installation script
-curl -o script.zip https://github.com/JerryKott/stash/tree/master/scripts/macx.zip
+curl -o script.zip https://github.com/JerryKott/stash/blob/master/scripts/macx.zip
 unzip script.zip
 chmod a+x macx/stash
 
@@ -27,7 +27,7 @@ cp -a macx/stash $TARGET/
 cp -a stash.im $TARGET/
 
 #Get and unpack examples.
-curl -o examples.zip https://github.com/JerryKott/stash/tree/master/examples.zip
+curl -o examples.zip https://github.com/JerryKott/stash/blob/master/examples.zip
 unzip examples.zip
 mv examples $TARGET/
 
